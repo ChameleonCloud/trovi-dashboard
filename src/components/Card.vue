4844c4212c073cc143b1f179642e399ceb04e414
alicesquivel <ace6qv@mail.missouri.edu>
2024-07-29 07:22:54 -0500

    Main cards update

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    bg: {
+        type: String,
+        default: 'bg-gray-50'
+
+    }
+});
+
+</script>
+<template>
+    <div :class="`${bg} p-6 rounded-lg shadow-md`">
+        <slot></slot>
+    </div>
+
+</template>
