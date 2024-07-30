3bf8ba94cc798342cbb2868fb437440047c6fbee
alicesquivel <ace6qv@mail.missouri.edu>
2024-07-30 14:03:52 -0500

    router setup

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,8 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
-import Hero from '@/components/Hero.vue';
-import HomeCards from '@/components/HomeCards.vue';
-import JobListing from '@/components/JobListings.vue';
 </script>
 
 <template>
   <Navbar />
-  <Hero />
-  <HomeCards />
-  <job-listing :limit="3" :showButton="true" />
+
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListing from '@/components/JobListings.vue';
+
+</script>
+<template>
+    <Hero />
+    <HomeCards />
+    <job-listing :limit="3" :showButton="true" />
+</template>
