07c74b991ae1db7a62b21c2a0fdced54da8b23b9
alicesquivel <ace6qv@mail.missouri.edu>
2024-07-30 13:07:07 -0500

    Jobs limit to three

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -28,6 +28,7 @@ defineProps({
                     <i class="fa-solid fa-location-dot text-lg"></i>
                     {{ job.location }}
                 </div>
+                <!-- concatenate with jobid -->
                 <a :href="'/job/' + job.id"
                     class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                     Read More
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,7 +1,11 @@
 <script setup>
 import jobsData from '@/jobs.json';
-import { ref } from 'vue';
+import { ref, defineProps } from 'vue';
 import JobListing from './JobListing.vue';
+
+defineProps({
+    limit: Number
+})
 
 const jobs = ref(jobsData);
 console.log(jobs.value);
@@ -9,12 +13,13 @@ console.log(jobs.value);
 
 <template>
     <section class="bg-blue-50 px-4 py-10">
-        <div class="container xl lg:container m-auto">
+        <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
-            <div class="grid.grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <!-- Iterate over jobs array -->
+                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
