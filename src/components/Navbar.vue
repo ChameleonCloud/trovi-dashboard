ad382366f3f6a4c7c0fbf16dc15bbceafa25d7bf
alicesquivel <ace6qv@mail.missouri.edu>
2024-07-30 14:45:50 -0500

    Navbar and router

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,30 @@
 <!-- composition API -->
 <script setup>
 import logo from '@/assets/img/logo.png'
+import { RouterLink } from 'vue-router';
+
 
 </script>
 
 <template>
-  <nav class="bg-[#7AC555] border-b border-green-500">
+  <nav class="bg-green-700 border-b border-green-500">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="flex h-20 items-center justify-between">
         <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
           <!-- Logo -->
-          <a class="flex flex-shrink-0 items-center mr-4" href="index.html">
+          <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
             <img class="h-10 w-auto" :src="logo" alt="Trovi" />
             <span class="hidden md:block text-white text-2xl font-bold ml-2">Trovi</span>
-          </a>
+          </RouterLink>
+
           <div class="md:ml-auto">
             <div class="flex space-x-2">
-              <a href="index.html"
-                class="text-white bg-[#7AC555] hover:bg-[#F5F5F5] hover:text-[#7AC555] rounded-md px-3 py-2">
-                Home
-              </a>
-              <a href="artifacts.html" class="text-white hover:bg-[#7AC555] hover:text-[#F5F5F5] rounded-md px-3 py-2">
-                Artifacts
-              </a>
-              <a href="add-artifact.html"
-                class="text-white hover:bg-[#7AC555] hover:text-[#F5F5F5] rounded-md px-3 py-2">
-                Add Artifact
-              </a>
+              <RouterLink to="/" class="text-white bg-green-900 hover:bg-gray-900 
+                hover:text-white rounded-md px-3 py-2">Home</RouterLink>
+              <RouterLink to="/jobs" class="text-white hover:bg-green-900 
+              hover:text-white rounded-md px-3 py-2">Jobs</RouterLink>
+              <RouterLink to="/jobs/add" class="text-white hover:bg-green-900 
+              hover:text-white rounded-md px-3 py-2">Add Job</RouterLink>
             </div>
           </div>
         </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import JobsView from "@/views/JobsView.vue";
 
 const router = createRouter({
     // to go back with the back button
@@ -11,11 +12,15 @@ const router = createRouter({
             name: 'home',
             component: HomeView,
         },
+        {
+            path: '/jobs',
+            name: 'jobs',
+            component: JobsView,
+        },
     ],
 });
 
 export default router;
-
 
 // home page to home vue
 // main router about page to about view
