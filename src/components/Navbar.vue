319348c3d338b61749ee5b31331645844fdadd62
alicesquivel <ace6qv@mail.missouri.edu>
2024-08-02 09:12:51 -0500

    Update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,11 +29,11 @@ const isActiveLink = (routePath) => {
               </RouterLink>
               <RouterLink to="/jobs"
                 :class="[isActiveLink('/jobs') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']">
-                Jobs
+                Explore <!-- Jobs -->
               </RouterLink>
               <RouterLink to="/jobs/add"
                 :class="[isActiveLink('/jobs/add') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']">
-                Add Job
+                Docs <!-- Add Job -->
               </RouterLink>
             </div>
           </div>
