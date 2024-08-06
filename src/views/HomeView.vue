0ab121ddefd419794ff41df392fa04a186218bc5
alicesquivel <ace6qv@mail.missouri.edu>
2024-08-06 17:46:23 -0500

    Pulled all the artifacts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,10 @@
 import Hero from '@/components/Hero.vue';
 import HomeCards from '@/components/HomeCards.vue';
 import JobListings from '@/components/ArtifactListings.vue';
-import SearchBar from '@/components/SearchBar.vue';
 </script>
 
 <template>
   <Hero />
-  <SearchBar />
   <HomeCards />
   <JobListings :limit="6" :showButton="true" />
 </template>
