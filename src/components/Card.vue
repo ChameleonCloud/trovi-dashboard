5250407f6e77067f0d08f2502e5efabeb602b724
Alicia Esquivel <ace6qv@mail.missouri.edu>
2024-08-06 10:20:27 -0500

    Add files via upload
    Update components

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,21 +2,15 @@
 import { defineProps } from 'vue';
 
 defineProps({
-    bg: {
-        type: String,
-        default: 'bg-gray-100'
-    }
+  bg: {
+    type: String,
+    default: 'bg-gray-100',
+  },
 });
-
 </script>
 
 <template>
-    <!-- bind to make it dynamic -->
-    <div :class="`${bg} p-6 rounded-lg shadow-md`">
-        <slot>
-
-        </slot>
-
-    </div>
-
+  <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
 </template>
